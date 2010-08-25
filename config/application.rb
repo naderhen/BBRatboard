28abee6128334cf5ab1dacced5ccc521ad53c7f9
Nader Hendawi <naderhen@Nader-Hendawis-iMac.local>
2010-08-25 14:23:39 -0400

    pdfkit

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
+require 'pdfkit'
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -33,7 +33,7 @@ module Bbv4
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
+    config.middleware.use PDFKit::Middleware
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
     # Configure sensitive parameters which will be filtered from the log file.
---
+++ config/initializers/pdfkit.rb
@@ -0,0 +1,13 @@
+
+# config/initializers/pdfkit.rb
+
+
+PDFKit.configure do |config|     
+
+
+config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+
+
+end
+
+
