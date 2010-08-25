0235e798503ceb3ba934c3abd8605680d8d9bc35
Nader Hendawi <naderhen@Nader-Hendawis-iMac.local>
2010-08-25 14:45:18 -0400

    another

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,8 @@
-PDFKit.configure do |config|     
-  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+# config/initializers/pdfkit.rb
+PDFKit.configure do |config|
+  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
+  config.default_options = {
+    :page_size => 'Legal',
+    :print_media_type => true
+  }
 end
-
-
