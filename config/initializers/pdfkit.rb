6dc81a44912750715b4767deb29f7aa356d1d3c5
Nader Hendawi <naderhen@Nader-Hendawis-iMac.local>
2010-08-25 14:29:09 -0400

    pdfkit again

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,13 +1,5 @@
-
-# config/initializers/pdfkit.rb
-
-
 PDFKit.configure do |config|     
-
-
-config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
-
-
+  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
 end
 
 
