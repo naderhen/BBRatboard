31fb8acab4d3c6c5745c5d7f65eea065cd3f57b0
Nader Hendawi <naderhen@Nader-Hendawis-iMac.local>
2010-08-25 14:37:32 -0400

    another

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Bbv4
   class Application < Rails::Application
-    require 'pdfkit'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
