4a3613455ba2db65adc583d8bd47226458015424
Nader Hendawi <naderhen@Nader-Hendawis-iMac.local>
2010-08-25 15:32:04 -0400

    hm

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -7,7 +7,7 @@ class ReportMailer < ActionMailer::Base
   def distribute_report(report,user,kit) 
     @report = report
     @user = user
-    attachments['report.pdf']=kit.to_file
+    attachments['report.pdf']=kit.to_file('/path/to/save/pdf')
     mail(:to => user.email, :subject=>"Testing")
   end
 end
