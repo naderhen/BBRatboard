59a3e069cdcb6f09b8d35e051936e5b04b84ca6e
Nader Hendawi <naderhen@gmail.com>
2010-10-04 12:44:52 -0400

    seeds

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -1,4 +1,5 @@
-require 'fastercsv'
+#require 'fastercsv'
+require 'csv'
 class CsvController < ApplicationController
   def import
   end
@@ -6,7 +7,7 @@ class CsvController < ApplicationController
   def upload
     table = ImportTable.new :original_path => params[:upload][:csv].original_path
     row_index = 0
-    FasterCSV.parse(params[:upload][:csv]) do |cells|
+    CSV.parse(params[:upload][:csv]) do |cells|
       column_index = 0
       cells.each do |cell|
         table.import_cells.build :column_index => column_index, :row_index => row_index, :contents => cell
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+puts 'SETTING UP DEFAULT ROLES'
+role1 = Role.create! :name=>"Admin"
+puts 'New role created: ' << role1.name
+role2 = Role.create! :name=>"Salesperson"
+puts 'New role created: ' << role2.name
+role3 = Role.create! :name=>"Observer"
+puts 'New role created: ' << role3.name
+role4 = Role.create! :name=>"Shipper"
+puts 'New role created: ' << role4.name
+
+
