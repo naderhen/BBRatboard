f525955e53fc954de3e7d8e4a5466a8ae5d8510d
Nader Hendawi <naderhen@Nader-Hendawis-iMac-2.local>
2010-10-07 12:04:50 -0400

    hm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,8 +44,6 @@ class ApplicationController < ActionController::Base
       end
     end
     
-    
-    protected
         def render_optional_error_file(status_code)
           status = interpret_status(status_code)
           render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'application.html.erb'
