5dc8b2eb920e3fc34d05088eab63c7b24219ea14
Nader Hendawi <naderhen@Nader-Hendawis-iMac-2.local>
2010-11-11 13:00:44 -0500

    remove initializer

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,8 @@
 # config/initializers/pdfkit.rb
-PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
-  config.default_options = {
-    :page_size => 'Legal',
-    :print_media_type => true
-  }
-end
+#PDFKit.configure do |config|
+#  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
+#  config.default_options = {
+#    :page_size => 'Legal',
+#    :print_media_type => true
+#  }
+#end
