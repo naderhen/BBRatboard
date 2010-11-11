42a32fae938e0195471bda60bb28164721ac25dd
Nader Hendawi <naderhen@Nader-Hendawis-iMac-2.local>
2010-11-11 14:41:57 -0500

    fix route

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,4 @@
 # config/initializers/pdfkit.rb
-#PDFKit.configure do |config|
-#  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
-#  config.default_options = {
-#    :page_size => 'Legal',
-#    :print_media_type => true
-#  }
-#end
+PDFKit.configure do |config|
+  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-i386').to_s
+end
