a5b714415cec22c23a4d04d016b09beeeb726c05
Nader Hendawi <naderhen@gmail.com>
2010-12-01 13:37:14 -0500

    availdate conditional

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -54,7 +54,7 @@ class BoardsController < ApplicationController
 
     respond_to do |format|
       if @board.save
-        format.html { redirect_to(@board, :notice => 'Board was successfully created.') }
+        format.html { redirect_to(root_url, :notice => 'Board was successfully created.') }
         format.xml  { render :xml => @board, :status => :created, :location => @board }
       else
         format.html { render :action => "new" }
@@ -70,7 +70,7 @@ class BoardsController < ApplicationController
 
     respond_to do |format|
       if @board.update_attributes(params[:board])
-        format.html { redirect_to(@board, :notice => 'Board was successfully updated.') }
+        format.html { redirect_to(root_url, :notice => 'Board was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
