e20ba901478126dc2171a7a8a5fedbb726526392
Nader Hendawi <naderhen@gmail.com>
2010-12-29 11:26:51 -0500

    edit pnotes if user

--- app/controllers/pnotes_controller.rb
+++ app/controllers/pnotes_controller.rb
@@ -44,7 +44,7 @@ class PnotesController < ApplicationController
 
     respond_to do |format|
       if @pnote.save
-        format.html { redirect_to(@pnote.preport, :notice => 'Pnote was successfully created.') }
+        format.html { redirect_to(@pnote.preport, :notice => 'Note was successfully created.') }
         format.xml  { render :xml => @pnote.preport, :status => :created, :location => @pnote }
       else
         format.html { render :action => "new" }
@@ -61,7 +61,7 @@ class PnotesController < ApplicationController
     
     respond_to do |format|
       if @pnote.update_attributes(params[:pnote])
-        format.html { redirect_to(@pnote.preport, :notice => 'Pnote was successfully updated.') }
+        format.html { redirect_to(@pnote.preport, :notice => 'Note was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
